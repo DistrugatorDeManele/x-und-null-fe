b8dd43d717eed7a114de630a1a1ac16d60081f41
DistrugatorDeManele <radustoican1@gmail.com>
2021-08-04 18:40:00 +0300

    1 file modified

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 export default class Homepage extends React.Component() {
-  constructor(props){
+  constructor(props) {
     super(props);
     var link = '';
   }
-  genereaza(length) {
-    link = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 1; i <= length; i++) {
-      link += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-  }
-  return (
-    <nav>
+
+  render() {
+    return (
       <div>
         <input
           id="buton"
@@ -26,6 +20,6 @@ export default class Homepage extends React.Component() {
         <br />
         <Link to="">Enter session</Link>
       </div>
-    </nav>
-  );
+    );
+  }
 }
