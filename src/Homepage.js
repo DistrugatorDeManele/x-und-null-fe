2abfeecb806740b071e71adda2a889f0d5fc75ca
DistrugatorDeManele <radustoican1@gmail.com>
2021-08-04 18:50:55 +0300

    1 file modified

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Homepage extends React.Component() {
+export default class Homepage extends React.Component{
   constructor(props) {
     super(props);
     var link = '';
   }
-
+  genereaza(length) {
+    link = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 1; i <= length; i++) {
+      link += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  }
   render() {
     return (
       <div>
