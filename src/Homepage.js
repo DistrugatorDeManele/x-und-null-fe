377b6ad1ea66e4452410e3cce61ccac418b88e19
DistrugatorDeManele <radustoican1@gmail.com>
2021-08-04 18:25:40 +0300

    1 file modified

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,8 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export default function Homepage() {
+  var link;
   return (
-    <div>
-    </div>
+    <nav>
+      <div>
+        <input
+          id="buton"
+          type="button"
+          value="Invite Link"
+          onClick={genereaza(10)}
+        />
+        {link}
+        <br />
+        <Link to="">Enter session</Link>
+      </div>
+    </nav>
   );
+  function genereaza(length) {
+    link = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 1; i <= length; i++) {
+      link += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  }
 }
