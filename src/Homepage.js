5127e681f408bd94bb54226e7a70925beeffc6ce
DistrugatorDeManele <radustoican1@gmail.com>
2021-08-04 22:33:20 +0300

    1 file modified

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 export default class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       link: ''
-    }
+    };
+    this.genereaza = this.genereaza.bind(this);
   }
-  genereaza(){
+  genereaza() {
     var link1 = '';
     var characters =
       'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 1; i <= 10; i++) {
-      link1 += characters.charAt(
-        Math.floor(Math.random() * charactersLength)
-      );
+      link1 += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    this.setState({link : link1})
+    this.setState({ link: link1 });
   }
   render() {
     return (
       <div>
         <p> {this.state.link} </p>
-        <button onClick = {this.genereaza}>Change Invite Link</button>
+        <button onClick={this.genereaza}>Change Invite Link</button>
         <br />
         <Link
           to={{
-          pathname: "/game",
-          search: this.state.link,
-          state: { fromDashboard: true }
+            pathname: '/game',
+            search: this.state.link,
+            state: { fromDashboard: true }
           }}
-        >Enter Session</Link>
+        >
+          Enter Session
+        </Link>
       </div>
     );
   }
-
 }
